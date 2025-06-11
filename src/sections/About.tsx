8c53acc12cf8de086a5b13678ce49d9687fdb32b
Jeff <125052610+jeff203-del@users.noreply.github.com>
2025-06-11 17:28:56 +0300

    Update About.tsx

--- src/sections/About.tsx
+++ src/sections/About.tsx
@@ -10,7 +10,7 @@ const About = () => {
         <div className="flex flex-col md:flex-row items-center md:items-start gap-10">
           <div className="w-48 h-48 rounded-full overflow-hidden flex-shrink-0 border-4 border-secondary">
             <img
-              src="image.jpg.JPG"
+              src="pfp.jpg.png"
               alt="Jeff Mutembei"
               className="w-full h-full object-cover"
             />
