4d0ea20aa0d32d60f48317c06c3593643af7f623
Jeff <125052610+jeff203-del@users.noreply.github.com>
2026-08-15 14:44:35 +0300

    Update logo for Kahustle experience entry

--- src/sections/Experience.tsx
+++ src/sections/Experience.tsx
@@ -2,7 +2,7 @@ const Experience = () => {
   const experiences = [
     {
       company: "Kahustle(Intern)",
-      logo: "https://kahustle.co.ke/wp-content/uploads/2025/04/350-LOGO-2.png",
+      logo: "logolog.jpg",
       role: "Full-Stack Developer & IT Systems Lead",
       period: "2025 May-August",
       description: "Supported the development and testing of Kahustle, a Kenyan online classifieds platform, focusing on user experience, functionality, and feature enhancements."
