d417af812b0036834051cf785d9e3c9f6d28048a
Jeff <125052610+jeff203-del@users.noreply.github.com>
2026-08-15 15:42:16 +0300

    Update Hero.tsx

--- src/sections/Hero.tsx
+++ src/sections/Hero.tsx
@@ -17,7 +17,7 @@ const Hero = () => {
             Hi, I'm Jeff Mutembei
           </h1>
           <p className="text-2xl md:text-3xl mb-8 text-secondary">
-            Software Developer & UI/UX Enthusiast
+            Software Engineer & IT Profession
           </p>
           <div className="mt-8">
             <Link
